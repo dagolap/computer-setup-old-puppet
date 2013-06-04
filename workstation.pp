6432c44af56b6307fddad7e7a5b9230dc5acab81
Dag Olav Prestegarden <dagolav@prestegarden.com>
2013-06-04 22:34:04 +0200

    Adding puppet config for default laptop setup

---
+++ workstation.pp
@@ -0,0 +1,58 @@
+$user = "dagolap"
+$user_group = "dagolap"
+$user_realname = "Dag Olav Prestegarden"
+$user_email = "dagolav@prestegarden.com"
+
+Exec {
+  path => [ "/bin", "/usr/bin", "/sbin", "/usr/sbin" ]
+}
+
+Package {
+  ensure => present
+}
+
+File {
+  owner => "$user",
+  group => "$user_group"
+}
+
+#####
+# Desktop use
+#####
+include googlechrome
+package { "browsers":
+  name => [ "chromium-browser", "firefox" ]
+}
+
+#####
+# Developer Tools
+#####
+# Version Control Systems
+package { "vcs":
+  name => [ "git" ]
+}
+
+file { "gitconfig":
+  path => "/home/$user/.gitconfig",
+  content => "[user]\n\tname = $user_fullname\n\temail = $user_email\n",
+}
+
+# Editors
+package { "editors":
+  name => [ "vim-gtk", "emacs24" ]
+}
+
+# Virtualization
+package { "virtualization":
+  name => [ "virtualbox", "vagrant" ]
+}
+
+# Tools
+package { "tools":
+  name => [ "screen", "mosh" ]
+}
+
+# Python
+package { "python":
+  name => [ "python", "python-virtualenv", "virtualenvwrapper" ]
+}
