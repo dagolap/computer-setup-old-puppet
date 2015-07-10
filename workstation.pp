782c0b5a7cd3460227a57ea74c5d79105dbbbf53
Dag Olav Prestegarden <dagolav@prestegarden.com>
2015-07-10 21:55:13 +0200

    Legger til nodejs og gjør det før apt-get update

--- workstation.pp
+++ workstation.pp
@@ -19,15 +19,16 @@ File {
 }
 
 #####
-# Add ppas before installing anything
+# Add ppas, anb do it before apt-get update
 #####
-Apt::Ppa{} -> Package <| |>
+Apt::Ppa{} -> Exec["apt-update"]
 apt::ppa { "ppa:ubuntu-mozilla-daily/firefox-aurora": }
 
 #####
 # Add debian repos without using ppa mechanism
 #####
-Apt::Source{} -> Package <| |>
+Apt::Source{} -> Exec["apt-update"] #Apply all apt source operations before apt-get update
+
 apt::source { 'synapse-core':
   comment  => 'Synapse quick launcher',
   location => 'http://ppa.launchpad.net/synapse-core/ppa/ubuntu',
@@ -35,6 +36,20 @@ apt::source { 'synapse-core':
   repos    => 'main',
   key      => {
     'id'     => '3DD99294193A09EF9270EEAFE4010F076C2225A4',
+    'server' => 'keyserver.ubuntu.com',
+  },
+  include  => {
+    'src' => true,
+    'deb' => true,
+  },
+}
+apt::source { 'nodejs':
+  comment  => 'Nodejs and NPM Javascript tools',
+  location => 'https://deb.nodesource.com/node_0.12/',
+  release  => 'vivid',
+  repos    => 'main',
+  key      => {
+    'id'     => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
     'server' => 'keyserver.ubuntu.com',
   },
   include  => {
@@ -79,6 +94,9 @@ package {["tmux", "mosh"]:}
 
 # Python
 package {["python", "python-virtualenv", "virtualenvwrapper"]:}
+
+# Javascript
+package {["nodejs"]:}
 
 exec {"install-dotfiles":
   command => "/home/$user/.computer-setup/setup-dotfiles",
