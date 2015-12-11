a32cc43703a7ad28d675344a7115981bfbe9e315
Dag Olav Prestegarden <dagolav@prestegarden.com>
2015-12-11 22:56:42 +0100

    Updating nodejs repo version

--- workstation.pp
+++ workstation.pp
@@ -47,8 +47,8 @@ apt::source { 'synapse-core':
 }
 apt::source { 'nodejs':
   comment  => 'Nodejs and NPM Javascript tools',
-  location => 'https://deb.nodesource.com/node_0.12',
-  release  => 'vivid',
+  location => 'https://deb.nodesource.com/node_4.x',
+  release  => 'wily',
   repos    => 'main',
   key      => {
     'id'     => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
